2225fce206b5b8861031e0a8fd775a711b823a44
Hugo <hugo@IOSMaster.local>
2015-07-21 15:41:14 +0800

    初始化项目

---
+++ weibo/weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weibo
+//
+//  Created by Hugo on 15/7/21.
+//  Copyright (c) 2015年 Hugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weibo/weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weibo
+//
+//  Created by Hugo on 15/7/21.
+//  Copyright (c) 2015年 Hugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
